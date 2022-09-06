1e8f121dae8e0f76f2147a90f3fba33c23411cef
rafaele85 <82197378+rafaele85@users.noreply.github.com>
2022-09-06 06:06:59 +0300

    convert main.js -> ts (#4)

--- src/main.js
+++
@@ -1,6 +0,0 @@
-const main = () => {
-    console.log('-----')
-}
-
-void main()
-
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+const a = 1
+
+export const main = () => {
+    console.log('-----fgfg')
+    console.log('-----fgfg2')
+}
+
+void main()
+
